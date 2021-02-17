b111772aaae3b93da6bb6b1ae0a0280b7a05607f
Forrest Erickson <forresterickson@comcast.net>
2021-02-17 18:32:57 -0500

    Autoformat code.

--- WinkNoBlock.ino
+++ WinkNoBlock.ino
@@ -1,42 +1,37 @@
 /**
- * File: WinkNoBlock
- * Modified by: Forrest Lee Erickson
- * Date: 20210217
- * 
- */
- 
+   File: WinkNoBlock
+   Modified by: Forrest Lee Erickson
+   Date: 20210217
+*/
+
 //For ESP32 Dev Kit on board blue LED.
 const int onBoardLED = 2;
-const int highTimeLED = 900; 
-const int lowTimeLED = 100; 
-
+const int highTimeLED = 900;
+const int lowTimeLED = 100;
 int lastLEDtime = 0;
 int nextLEDchange = 100; //time in ms.
 
-
-
 void setup() {
   // put your setup code here, to run once:
+
   pinMode(onBoardLED, OUTPUT);      // set the LED pin mode
   digitalWrite(onBoardLED, HIGH);   // turn the LED on (HIGH is the voltage level)
-
-}
+}//end setup
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-    //Wink the LED
-  if ((millis()-lastLEDtime)>nextLEDchange){
-    if(digitalRead(onBoardLED)==LOW){
-     digitalWrite(onBoardLED, HIGH);   // turn the LED on (HIGH is the voltage level)
-     lastLEDtime = millis();
-     nextLEDchange = highTimeLED; 
-    }else{
+  //Wink the LED
+  if ((millis() - lastLEDtime) > nextLEDchange) {
+    if (digitalRead(onBoardLED) == LOW) {
+      digitalWrite(onBoardLED, HIGH);   // turn the LED on (HIGH is the voltage level)
+      lastLEDtime = millis();
+      nextLEDchange = highTimeLED;
+    } else {
       digitalWrite(onBoardLED, LOW);   // turn the LED on (HIGH is the voltage level)
-     lastLEDtime = millis();
-     nextLEDchange = lowTimeLED; 
-    }     
-  }//LED change
+      lastLEDtime = millis();
+      nextLEDchange = lowTimeLED;
+    }
+  }//end LED wink
 
-
-}
+}//End loop
