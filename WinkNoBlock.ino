1aff09004ebfb106158bfb9093a8dc845287997f
Forrest Erickson <forresterickson@comcast.net>
2021-02-22 13:23:45 -0500

    20210222 Modifed for long type to hold millis() results and check for millis wrap around.

--- WinkNoBlock.ino
+++ WinkNoBlock.ino
@@ -1,15 +1,16 @@
 /**
    File: WinkNoBlock
    Modified by: Forrest Lee Erickson
-   Date: 20210217
+   Date: 20210217 Initial release.
+   Date: 20210222 Modifed for long type to hold millis() results and check for millis wrap around.
 */
 
 //For ESP32 Dev Kit on board blue LED.
 const int onBoardLED = 2;
 const int highTimeLED = 900;
 const int lowTimeLED = 100;
-int lastLEDtime = 0;
-int nextLEDchange = 100; //time in ms.
+long lastLEDtime = 0;
+long nextLEDchange = 100; //time in ms.
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,7 +23,7 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   //Wink the LED
-  if ((millis() - lastLEDtime) > nextLEDchange) {
+  if (((millis() - lastLEDtime) > nextLEDchange)||(millis()< lastLEDtime)) {
     if (digitalRead(onBoardLED) == LOW) {
       digitalWrite(onBoardLED, HIGH);   // turn the LED on (HIGH is the voltage level)
       lastLEDtime = millis();
