88f01a6b9f5c7c785e7317e7691a261f811e5727
Forrest Erickson <forresterickson@comcast.net>
2021-03-04 09:18:32 -0500

    Change to uppercase for constants. Move lastLEDtim update in loop.

--- WinkNoBlock.ino
+++ WinkNoBlock.ino
@@ -3,20 +3,22 @@
    Modified by: Forrest Lee Erickson
    Date: 20210217 Initial release.
    Date: 20210222 Modifed for long type to hold millis() results and check for millis wrap around.
+   Date: 20210304 Make constants all upper case.
 */
 
 //For ESP32 Dev Kit on board blue LED.
-const int onBoardLED = 2;
-const int highTimeLED = 900;
-const int lowTimeLED = 100;
+//const int ON_BOARD_LED = 2;    // ESP32 Kit
+const int ON_BOARD_LED = 13;    //Arduino UNO
+const int HIGH_TIME_LED = 900;
+const int LOW_TIME_LED = 100;
 long lastLEDtime = 0;
 long nextLEDchange = 100; //time in ms.
 
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(onBoardLED, OUTPUT);      // set the LED pin mode
-  digitalWrite(onBoardLED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  pinMode(ON_BOARD_LED, OUTPUT);      // set the LED pin mode
+  digitalWrite(ON_BOARD_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
 }//end setup
 
 void loop() {
@@ -24,15 +26,14 @@ void loop() {
 
   //Wink the LED
   if (((millis() - lastLEDtime) > nextLEDchange)||(millis()< lastLEDtime)) {
-    if (digitalRead(onBoardLED) == LOW) {
-      digitalWrite(onBoardLED, HIGH);   // turn the LED on (HIGH is the voltage level)
-      lastLEDtime = millis();
-      nextLEDchange = highTimeLED;
+    if (digitalRead(ON_BOARD_LED) == LOW) {
+      digitalWrite(ON_BOARD_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+      nextLEDchange = HIGH_TIME_LED;
     } else {
-      digitalWrite(onBoardLED, LOW);   // turn the LED on (HIGH is the voltage level)
-      lastLEDtime = millis();
-      nextLEDchange = lowTimeLED;
+      digitalWrite(ON_BOARD_LED, LOW);   // turn the LED on (HIGH is the voltage level)
+      nextLEDchange = LOW_TIME_LED;
     }
+    lastLEDtime = millis();
   }//end LED wink
 
 }//End loop
