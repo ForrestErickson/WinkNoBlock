f9120683d43f8b7d9d07e146e02df1857c6a2389
Forrest Erickson <forresterickson@comcast.net>
2021-05-05 16:47:47 -0400

    Rename ON_BOARD_LED to LED_BUILTIN to use the name in Arduino.h

--- WinkNoBlock.ino
+++ WinkNoBlock.ino
@@ -7,8 +7,8 @@
 */
 
 //For ESP32 Dev Kit on board blue LED.
-//const int ON_BOARD_LED = 2;    // ESP32 Kit
-const int ON_BOARD_LED = 13;    //Arduino UNO
+//const int LED_BUILTIN = 2;    // ESP32 Kit
+const int LED_BUILTIN = 13;    //Arduino UNO
 const int HIGH_TIME_LED = 900;
 const int LOW_TIME_LED = 100;
 long lastLEDtime = 0;
@@ -17,8 +17,8 @@ long nextLEDchange = 100; //time in ms.
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(ON_BOARD_LED, OUTPUT);      // set the LED pin mode
-  digitalWrite(ON_BOARD_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  pinMode(LED_BUILTIN, OUTPUT);      // set the LED pin mode
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 }//end setup
 
 void loop() {
@@ -26,11 +26,11 @@ void loop() {
 
   //Wink the LED
   if (((millis() - lastLEDtime) > nextLEDchange)||(millis()< lastLEDtime)) {
-    if (digitalRead(ON_BOARD_LED) == LOW) {
-      digitalWrite(ON_BOARD_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+    if (digitalRead(LED_BUILTIN) == LOW) {
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
       nextLEDchange = HIGH_TIME_LED;
     } else {
-      digitalWrite(ON_BOARD_LED, LOW);   // turn the LED on (HIGH is the voltage level)
+      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
       nextLEDchange = LOW_TIME_LED;
     }
     lastLEDtime = millis();
